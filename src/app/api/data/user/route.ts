9807ba1b7d066368acf757afd2b861a0b399e805
sohanemon <m.sohanemon@gmail.com>
2023-04-21 10:36:36 +0600

    prisma creation done

--- src/app/api/data/user/route.ts
+++ src/app/api/data/user/route.ts
@@ -2,6 +2,22 @@ import prisma from '@/lib/prisma';
 import { NextResponse } from 'next/server';
 
 export async function GET(req: Request) {
-  const data = await prisma.user.findMany();
+  const data = await prisma.user.findMany({
+    select: { email: true },
+  });
+  return NextResponse.json(data);
+}
+
+export async function POST(req: Request) {
+  //   const data = await req.json();
+  const data = await prisma.user.create({
+    data: {
+      name: 'msr',
+      email: 'emon@3masf.com',
+    },
+    select: {
+      id: true,
+    },
+  });
   return NextResponse.json(data);
 }
